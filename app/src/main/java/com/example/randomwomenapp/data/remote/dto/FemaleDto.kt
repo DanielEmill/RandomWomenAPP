cf462aaef7c71c8020971076c24c70ded3ef76a4
Emill Daniel Garcia Castellanos <Danielemill2012@gmail.com>
2023-11-05 00:32:12 -0400

    Update

--- app/src/main/java/com/example/randomwomenapp/data/remote/dto/FemaleDto.kt
+++ app/src/main/java/com/example/randomwomenapp/data/remote/dto/FemaleDto.kt
@@ -1,11 +1,16 @@
 package com.example.randomwomenapp.data.remote.dto
 data class FemaleDto(
     val name: Name,
+    val picture: Picture,
     val nat: String
 )
-
 data class Name(
     val title: String,
     val first: String,
     val last: String
+)
+data class Picture(
+    val large: String,
+    val medium: String,
+    val thumbnail: String
 )
